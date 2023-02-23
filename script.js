8d83d892db8da3a1491e0df2f51d68bc2cfe1d9d
mrseu <126210202+mrseu@users.noreply.github.com>
2023-02-24 02:40:52 +0300

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+//console.log('12');
+
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Скоро пойдём кушать весёлого барана!';
+})
